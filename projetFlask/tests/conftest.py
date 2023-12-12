97f4777574ad79052d71cb2c94cfaf4e7ea8e920
MaximeMonterin <91959894+maximeMonterin@users.noreply.github.com>
2023-12-12 14:45:52 +0100

    Add files via upload

---
+++ projetFlask/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+@pytest.fixture
+def client():
+	from web import app
+	
+	app.app.config['TESTING'] = True
+	yield app.app.test_client()
---
+++ projetFlask/web/app.py
@@ -0,0 +1,22 @@
+from flask import request, Flask
+import json
+
+app = Flask(__name__)
+
+@app.route('/plus_one')
+def plus_one():
+	x = int(request.args.get('x', 1))
+	return json.dumps({'x':x+1})
+
+@app.route('/plus_two')
+def plus_two():
+	x = int(request.args.get('x', 1))
+	return json.dumps({'x':x+2})
+
+@app.route('/square')
+def square():
+	x = int(request.args.get('x', 1))
+	return json.dumps({'x':x*x})
+	
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0',port=5000)
